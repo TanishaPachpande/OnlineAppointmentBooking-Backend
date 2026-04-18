4cbd3109d7ca1b09701b97de9849f8cdde0f553b
Tanisha Pachpande <tanishapachpande86@gmail.com>
2026-04-18 11:01:56 +0530

    [Tanisha]: Modified Files

--- auth-service/src/main/java/com/medibook/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/medibook/auth/config/SecurityConfig.java
@@ -11,13 +11,13 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	    http
-	        .csrf(csrf -> csrf.disable())
-	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers("/auth/**").permitAll()  
-	            .anyRequest().permitAll()                  
-	        );
+		http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/auth/**").permitAll()
+						.anyRequest().authenticated()
+				);
 
-	    return http.build();
+		return http.build();
 	}
 }
